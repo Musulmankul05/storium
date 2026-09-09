f24546a1f7bb19da914b1a769e7285a76636de52
hikonakura <musulmankulmamadiev@gmail.com>
2026-09-09 13:49:28 +0600

    fix

--- routers/objects.py
+++ routers/objects.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException, Request
+from fastapi import APIRouter, Depends, HTTPException, Query, Request
 from fastapi.responses import StreamingResponse
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -7,11 +7,12 @@ from models import UserModel
 from schemas.objects import (
     BucketNameSchema,
     BucketSchema,
+    ObjectResponseSchema,
 )
 from services.depends import get_current_user
 from services.storage import StorageService
 
-router = APIRouter(prefix="/storage", tags=["Storage"])
+router = APIRouter(tags=["Storage"])
 
 
 @router.post("/buckets", status_code=201, response_model=BucketSchema)
@@ -35,25 +36,27 @@ async def get_buckets(
     storage = StorageService(db)
     return await storage.get_user_buckets(current_user.id)
 
+
 @router.delete("/buckets/{bucket_name}")
 async def delete_bucket(
     bucket_name: str,
     db: AsyncSession = Depends(get_session),
-    current_user: UserModel = Depends(get_current_user)
+    current_user: UserModel = Depends(get_current_user),
 ):
     storage = StorageService(db)
     try:
         await storage.del_bucket(bucket_name, current_user.id)
     except ValueError:
-        raise HTTPException(404, 'bucket not found')
+        raise HTTPException(404, "bucket not found")
     except FileExistsError:
-        raise HTTPException(409, 'bucket is not empty')
+        raise HTTPException(409, "bucket is not empty")
     return {"msg": f"bucket {bucket_name} delete success"}
 
-@router.put("/{bucket_name}/{key:path}", status_code=201)
+
+@router.put("/buckets/{bucket_name}/objects/{key:path}", status_code=201)
 async def put_object(
-    bucket_name,
-    key,
+    bucket_name: str,
+    key: str,
     request: Request,
     db: AsyncSession = Depends(get_session),
     current_user: UserModel = Depends(get_current_user),
@@ -73,17 +76,17 @@ async def put_object(
     return {"msg": "object created", "key": key}
 
 
-@router.get("/{bucket_name}/{key:path}")
+@router.get("/buckets/{bucket_name}/objects/{key:path}")
 async def get_object(
-    bucket_name,
-    key,
+    bucket_name: str,
+    key: str,
     db: AsyncSession = Depends(get_session),
     current_user: UserModel = Depends(get_current_user),
 ):
     storage = StorageService(db)
     try:
         obj, file_stream = await storage.get_object(bucket_name, key, current_user.id)
-    except ValueError, FileNotFoundError:
+    except (ValueError, FileNotFoundError):
         raise HTTPException(404, "object or bucket not found")
     return StreamingResponse(
         file_stream,
@@ -92,10 +95,10 @@ async def get_object(
     )
 
 
-@router.delete("/{bucket_name}/{key:path}", status_code=204)
+@router.delete("/buckets/{bucket_name}/objects/{key:path}", status_code=204)
 async def delete_object(
-    bucket_name,
-    key,
+    bucket_name: str,
+    key: str,
     db: AsyncSession = Depends(get_session),
     current_user: UserModel = Depends(get_current_user),
 ):
@@ -104,3 +107,21 @@ async def delete_object(
         await storage.del_object(bucket_name, key, current_user.id)
     except ValueError:
         raise HTTPException(404, "object not found")
+
+
+@router.get("/buckets/{bucket_name}/objects", response_model=list[ObjectResponseSchema,])
+async def get_objects_list(
+    bucket_name: str,
+    prefix: str | None = None,
+    limit: int = Query(default=20, ge=1, le=40),
+    offset: int = Query(default=0, ge=0),
+    current_user: UserModel = Depends(get_current_user),
+    db: AsyncSession = Depends(get_session)
+):
+    storage = StorageService(db)
+    try:
+        objects = await storage.get_bucket_objects(bucket_name, current_user.id, limit, offset, prefix)
+    except ValueError:
+        raise HTTPException(404, 'bucket not found')
+    return objects
+        
